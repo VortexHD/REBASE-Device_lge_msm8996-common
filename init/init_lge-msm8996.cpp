e10f5df53dece0185e41fa6243b66143ea4e17cf
arter97 <qkrwngud825@gmail.com>
2021-01-23 18:34:18 +0100

    msm8996-common: init: workaround SafetyNet check

--- init/init_lge-msm8996.cpp
+++ init/init_lge-msm8996.cpp
@@ -15,25 +15,71 @@
  */
 
 
-#include <cstdio>
+#include <cstdlib>
+#include <fstream>
+#include <string.h>
+#include <sys/sysinfo.h>
+#include <unistd.h>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <sys/_system_properties.h>
 
 #include "vendor_init.h"
+#include "property_service.h"
 
-void property_override(char const prop[], char const value[])
+using android::base::GetProperty;
+int property_set(const char *key, const char *value) {
+    return __system_property_set(key, value);
+}
+
+void property_override(char const prop[], char const value[], bool add = true)
 {
     prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
+    pi = (prop_info *) __system_property_find(prop);
     if (pi)
         __system_property_update(pi, value, strlen(value));
-    else
+    else if (add)
         __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+/* From Magisk@native/jni/magiskhide/hide_utils.c */
+static const char *cts_prop_key[] =
+        { "ro.boot.vbmeta.device_state", "ro.boot.verifiedbootstate", "ro.boot.flash.locked",
+          "ro.boot.veritymode", "ro.boot.warranty_bit", "ro.warranty_bit",
+          "ro.debuggable", "ro.secure", "ro.build.type", "ro.build.tags",
+          "ro.vendor.boot.warranty_bit", "ro.vendor.warranty_bit",
+          "vendor.boot.vbmeta.device_state", nullptr };
+
+static const char *cts_prop_val[] =
+        { "locked", "green", "1",
+          "enforcing", "0", "0",
+          "0", "1", "user", "release-keys",
+          "0", "0",
+          "locked", nullptr };
+
+static const char *cts_late_prop_key[] =
+        { "vendor.boot.verifiedbootstate", nullptr };
+
+static const char *cts_late_prop_val[] =
+        { "green", nullptr };
+
+static void workaround_cts_properties() {
+
+    // Hide all sensitive props
+    for (int i = 0; cts_prop_key[i]; ++i) {
+		property_override(cts_prop_key[i], cts_prop_val[i]);
+	}
+	for (int i = 0; cts_late_prop_key[i]; ++i) {
+		property_override(cts_late_prop_key[i], cts_late_prop_val[i]);
+    }
+	
 }
 
 void vendor_load_properties()
 {
+    // Workaround SafetyNet
+    workaround_cts_properties();
 }
