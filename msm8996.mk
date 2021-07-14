656e54c02bef63f01a5ce77e2bcc8e1e840b1ba7
Stealth1226 <rishawniyer06@gmail.com>
2021-07-14 03:37:00 +0000

    msm8996-common: Import GCamGO

--- msm8996.mk
+++ msm8996.mk
@@ -167,6 +167,10 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
+# GCam Go
+PRODUCT_PACKAGES += \
+    GCam
+
 # Gesture Handler
 PRODUCT_PACKAGES += \
     GestureHandler
